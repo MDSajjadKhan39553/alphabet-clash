5d2ed80dabe6143692e12b0d485c8536279ec00b
MD Sajjad Khan <mdsajjadkhan39553@gmail.com>
2025-01-21 14:53:41 +0600

    game almost complete

--- scripts/script.js
+++ scripts/script.js
@@ -13,6 +13,62 @@ function continueGame(){
 
 function play(){
     hideElementById("home-screen");
+    hideElementById("final-score");
     showElementById("play-ground");
+    setTextElementValueById("current-life", 10);
+    setTextElementValueById("current-score", 0);
     continueGame("");
+}
+document.addEventListener("keyup", handleKeyupEvent);
+
+function handleKeyupEvent(event){
+    const playerPressed = event.key;
+    if(playerPressed === "Escape"){
+        gameOver();
+    }
+    const currentAlphabetElement = document.getElementById("current-alphabet");
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+
+
+if(playerPressed === expectedAlphabet){
+
+    // const currentScoreElement = document.getElementById("current-score");
+    // const currentScoreText = currentScoreElement.innerText;
+    // const currentScore = parseInt(currentScoreText);
+    // const newScore = currentScore + 1;
+    // currentScoreElement.innerText = newScore;
+    currentScore = getTextelementValueById("current-score");
+    const newScore = currentScore + 1;
+    setTextElementValueById("current-score", newScore);
+    removeBackgroundColorById(expectedAlphabet);
+    continueGame();
+}
+else{
+    const currentLife = getTextelementValueById("current-life");
+    const newLife = currentLife - 1;
+    setTextElementValueById("current-life", newLife);
+    if(newLife === 0){
+        gameOver();
+    }
+    // if (currentLife => 0){
+    //     const currenrLifeElement = document.getElementById("current-life");
+    //     const currentLifeText = currenrLifeElement.innerText;
+    //     const currentLife = parseInt(currentLifeText);
+    //     const newLife = currentLife - 1;
+    //     currenrLifeElement.innerText = newLife;
+    //     }
+    //     else{
+    //         console.log("Game Over");
+    //     }
+}
+}
+
+function gameOver(){
+    hideElementById("play-ground");
+    showElementById("final-score");
+    lastScore = getTextelementValueById("current-score");
+    setTextElementValueById("last-score", lastScore);
+    const currentAlphabet = getElementTextById("current-alphabet");
+    removeBackgroundColorById(currentAlphabet);
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -21,3 +21,24 @@ function setBackgroundColorById(elementId){
     const element = document.getElementById(elementId);
     element.classList.add("bg-orange-400")
 }
+function removeBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove("bg-orange-400")
+}
+
+
+function getTextelementValueById(elementId){
+    const element = document.getElementById(elementId);
+    const valueText = element.innerText;
+    const value = parseInt(valueText);
+    return value;
+}
+function setTextElementValueById(elementId, value){
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
+function getElementTextById(elementId){
+    const element = document.getElementById(elementId);
+    const text = element.innerText;
+    return text;
+}
